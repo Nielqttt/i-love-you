263c313be8553625770173faf19b6da438661d51
Nielqttt <160033978+Nielqttt@users.noreply.github.com>
2024-02-14 22:15:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,9 +44,9 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
+    "Baby please",
+    "Don't do this to me baby:(",
+    "You're breaking my heart baby",
     "I'm gonna cry...",
   ];
 
